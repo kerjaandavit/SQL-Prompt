8c434912df1fd60e5a89a02144b8464387a564a7
Davit Hidayat <91250523+kerjaandavit@users.noreply.github.com>
2023-12-25 18:19:55 +0700

    Update SQLcontoh.sql
    Push prompt 1

--- SQLcontoh.sql
+++ SQLcontoh.sql
@@ -1,3 +1,9 @@
+-- Prompt 1
+SELECT post_id, (CAST(actions AS DECIMAL) / impressions * 100) AS popularity
+FROM linkedin_posts
+WHERE (CAST(actions AS DECIMAL) / impressions * 100) >= 1.0
+ORDER BY popularity DESC;
+
 
 -- Prompt 2
 SELECT First_Name
